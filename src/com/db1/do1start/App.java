6f6cfbe1b035546297c970a2ca161a4c15ba483f
leonardo <leonardonasser11@hotmail.com>
2019-11-11 16:57:57 -0200

    EX Integer 1

--- src/com/db1/do1start/App.java
+++ src/com/db1/do1start/App.java
@@ -1,14 +1,26 @@
 package com.db1.do1start;
 
 
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-  
-        System.out.println("hello");
+        Scanner ler = new Scanner(System.in);
+      Integer numero1,numero2,resultado;
+
+
+      System.out.println("Digite o numero 1: ");
+      numero1=ler.nextInt();
+
+      System.out.println("Digite o numero 2: ");
+      numero2=ler.nextInt();
+
+        resultado=numero1+numero2;
+        System.out.println("Resultado: "+resultado);
+        }
 
 
     }
 
-}
+
